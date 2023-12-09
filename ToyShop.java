948ef4bebb382b97b25c38442583c3dfe5cacc38
Margarita <diasimba2014@gmail.com>
2023-12-09 19:49:08 +0300

    загрузка данных из файла

--- ToyShop.java
+++ ToyShop.java
@@ -95,11 +95,61 @@ public void saveToFile(String filename) throws IOException {
     }
 }
 
+//Загрузка данных об игрушках из файла
 
+public void loadFromFile(String filename) throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File(filename)))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] toyData = line.split(",");
+            Toy toy = new Toy(Integer.parseInt(toyData[0]), toyData[1], Integer.parseInt(toyData[2]), Double.parseDouble(toyData[3]));
+            toys.add(toy);
 
+        }
 
+    }
+}
 
+class Toy {
 
+    private int id;
+    private String name;
+    private int quantity;
+    private double weight;
+
+    public Toy(int id, String name, int quantity, double weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+     
+    public int getQuantity() {
+        return quantity;
+
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
 
 
 
